e5d47c603a406c8f73c5f88792b18a9586fbe782
crabamongspies <windforce0000@outlook.com>
2025-05-23 16:02:39 -0400

    simplify

--- games/code.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("contextmenu", e => {
-  if (e.target.tagName === "IMG") e.preventDefault();
-});
-
-document.addEventListener("dragstart", e => {
-  if (e.target.tagName === "IMG") e.preventDefault();
-});
